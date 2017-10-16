88dd01255cd0e37358b6e9ac04b9dbeb95777210
Brian J Gravelle <gravelle@cs.uoregon.edu>
2017-10-16 19:23:34 +0000

    readme and some other stuff

--- control.py
+++ control.py
@@ -2,10 +2,13 @@ import time, socket, sys
 from datetime import datetime as dt
 import paho.mqtt.client as paho
 import signal
+import mqtt_init
 
-MY_NAME = 'Brian Gravelle' # change to your name
-fi = open("useless.txt", 'w')
-names = []
+class Control(object):
+    """docstring for Control"""
+    def __init__(self):
+        self.mqtt_topic = "650/Internet_of_Things/"
+        
 
 # Deal with control-c
 def control_c_handler(signum, frame):
@@ -25,9 +28,6 @@ ip_addr = str(s.getsockname()[0])
 print('IP address: {}'.format(ip_addr))
 s.close()
 
-def on_connect(client, userdata, flags, rc):
-    print('connected')
-
 # The callback for when a PUBLISH message is received from the server that matches any of your topics.
 # However, see note below about message_callback_add.
 def on_message(client, userdata, msg):
@@ -41,43 +41,7 @@ def on_message(client, userdata, msg):
         fi.write(name)
         fi.write("\n")
         names.append(name)
-    # TODO Here is where you write to file and unsubscribe
 
-# You can also add specific callbacks that match specific topics.
-# See message_callback_add at https://pypi.python.org/pypi/paho-mqtt#callbacks.
-# When you have add ins, then the on_message handler just deals with topics
-# you have *not* written an add in for. You can just use the single on_message
-# handler for this problem.
-
-def on_disconnect(client, userdata, rc):
-    print("Disconnected in a normal way")
-    #graceful so won't send will
-
-def on_log(client, userdata, level, buf):
-    print("log: {}".format(buf)) # only semi-useful IMHO
-
-
-def setup_mqtt(mqtt_topic, will):
-    # Instantiate the MQTT client
-    mqtt_client = paho.Client()
-
-    # set up handlers
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
-    mqtt_client.on_disconnect = on_disconnect
-    mqtt_client.on_log = on_log
-
-    mqtt_topic = 'cis650/' + socket.gethostname()  # don't change this or you will screw it up for others
-
-    # See https://pypi.python.org/pypi/paho-mqtt#option-functions.
-    mqtt_client.will_set(mqtt_topic, will, 0, False)
-
-    broker = 'sansa.cs.uoregon.edu'  # Boyana's server
-    # Public brokers: https://github.com/mqtt/mqtt.github.io/wiki/public_brokers, e.g., 'test.mosquitto.org'
-
-    mqtt_client.connect(broker, '1883')
-
-    return mqtt_client
 
 # You can subscribe to more than one topic: https://pypi.python.org/pypi/paho-mqtt#subscribe-unsubscribe.
 # If you do list more than one topic, consdier using message_callback_add for each topic as described above.
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Internet of Thrones
+
+### topic
+  * Internet_of_Thrones
+  * subtopics all lowercase
+  * control
+  * car
+  
+
