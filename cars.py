aa0e3de58e105c9a547c2f7afe9e85138713e3e4
enmccarthy <m.erin34@yahoo.com>
2017-10-18 13:32:00 -0700

    added obj to mqtt and finished car class

--- cars.py
+++ cars.py
@@ -1,59 +1,81 @@
-import mraa, time, sys
+#import mraa, time, sys
+import sys
 import paho.mqtt.client as paho
-import mqtt_client
+from mqtt_client import MQTTClient
+from threading import Thread
+from time import sleep
 
-N=3
+N=int(sys.argv[1])
+if N > 7:
+    quit
 
-class Car:
+class Car(MQTTClient):
     def __init__(self, number):
         self.isRiding = False
         self.hasPassengers = False
         self.ledNum = number+2
         self.name = "car" + str(number)
+        super(Car,self).__init__()
+        self.subscribe("test")
     def ride(self):
-        mqtt_.publish(self.name + ' riding')
+        self.publish('test',self.name + ' riding')
         self.isRiding = True
-        turnOnLed(self)
+        #turnOnLed(self)
     def request(self):
-        mqtt_.publish(self.name + ' requesting')
+        self.publish('test', self.name + ' requesting')
     def pickUp(self):
-        mqtt_.publish(self.name + ' pickup')
+        self.publish('test',self.name + ' pickup')
         self.hasPassengers = False
     def dropOff(self):
-        turnOffLed(self)
-        mqtt_.publish(self.name + ' dropoff')
+       # turnOffLed(self)
+        self.publish('test', self.name + ' dropoff')
         self.hasPassengers = False
         self.isRiding = False
+    @staticmethod
+    def on_message(client, userdata, msg, mqtt_client):
+        message = msg.payload
+        if message == 'Platform is full':
+            if mqtt_client.hasPassengers == False:
+                mqtt_client.request()
+        elif message == mqtt_client.name:
+            mqtt_client.pickUp()
+            mqtt_client.ride()
+            myThread = Thread(target=timeFunc, args=(mqtt_client,4))
+            myThread.start()
+        else:
+            print("not message")
 
+#car platform and bcast 
+def timeFunc(car, num):
+    sleep(num)
+    car.dropOff()
 def createCars(num):
     cars = []
 
     for i in range(num):
         cars.append(Car(i))
     return cars
-
+"""
 def turnOnLed(car):
     led = mraa.Gpio(car.ledNum)
     led.dir(mraa.DIR_OUT)
     led.write(0)
-
+"""
+"""
 def turnOffLed(car):
     led = mraa.Gpio(car.ledNum)
     led.dir(mraa.DIR_OUT)
     led.write(1)
-
-mqtt_= mqtt_client.MQTTClient()
+"""
 #mqtt_.subscribe('control')
-
-def on_message(client, userdata, msg, mqtt_client):
-    print("here")
 #on message 'ready for pick up' send request if not full or riding
 #if recieves a message allowed to pick up, pick up and begin riding
 #after some time drop off passengers
 
-cars = createCars(3)
-for car in cars:
-    turnOnLed(car)
-
+cars = createCars(N)
+#for car in cars:
+ #   turnOnLed(car)
+while True:
+    pass 
 
 
