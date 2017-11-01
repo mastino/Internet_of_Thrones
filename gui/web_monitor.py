b7ef79c95b2614d43e8fb5e484d670e6620fadcc
awh <andrew.woroby.hampton@gmail.com>
2017-11-01 16:11:24 -0700

    little bit of work on the web frontend

--- gui/web_monitor.py
+++ gui/web_monitor.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, render_template, url_for, request, redirect, jsonify
-import logging, time
+import logging, time, Queue, copy
 import atexit
 from mqtt_client import MQTTClient
 
@@ -9,12 +9,34 @@ app.debug = True
 app.logger.setLevel(logging.DEBUG)
 
 waiting_to_display = []
+last_ten_messages = Queue.Queue()
+property1_dump = []
+
+def add_to_last_ten(msg):
+    global last_ten_messages
+    if last_ten_messages.qsize() >= 10:
+        last_ten_messages.get()
+    last_ten_messages.put(msg)
 
 class Monitor(MQTTClient):
+
+    phil0_arise = False
 
     @staticmethod
     def on_message(client, userdata, msg, mqtt_client):
     	waiting_to_display.append( "[[%s]] %s" % (msg.topic, msg.payload) )
+        add_to_last_ten( "[[%s]] %s" % (msg.topic, msg.payload) )
+        mqtt_client.assert1(msg.topic.split('/')[-1], msg.payload)
+
+    def assert1(self, topic, msg):
+        if topic == 'butler':
+            action, phil_id = msg.split(':')
+            if action == 'eating' and phil_id == '0' and not self.phil0_arise:
+                temp = copy.copy(last_ten_messages)
+                while not temp.empty():
+                    property1_dump.append(temp.get())
+            elif action == 'arise' and phil_id == '0':
+                self.phil0_arise = True
 
 def monitor_cleanup(client):
     client.disconnect()
@@ -27,9 +49,14 @@ def index():
 @app.route("/_msgs")
 def msgs():
     global waiting_to_display
-    dump = waiting_to_display[:]
+    global property1_dump
+    if property1_dump:
+        res = jsonify(msgs=waiting_to_display, property1=property1_dump)
+        property1_dump = []
+    else:
+        res = jsonify(msgs=waiting_to_display)
     waiting_to_display = []
-    return jsonify(msgs=dump)
+    return res
 
 if __name__ == "__main__":
     monitor = Monitor()
