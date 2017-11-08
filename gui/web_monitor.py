7a6a8294df8865cbcf7b282c3706d0d6123f87f4
awh <andrew.woroby.hampton@gmail.com>
2017-11-07 18:46:40 -0800

    made second property reset on success

--- gui/web_monitor.py
+++ gui/web_monitor.py
@@ -23,13 +23,14 @@ class Monitor(MQTTClient):
 
     phil0_arise   = False
     phil0_sitdown = False
-    phil1_arise   = False
+    phil1_arise   = True
 
     @staticmethod
     def on_message(client, userdata, msg, mqtt_client):
     	waiting_to_display.append( "[[%s]] %s" % (msg.topic, msg.payload) )
         add_to_last_ten( "[[%s]] %s" % (msg.topic, msg.payload) )
         mqtt_client.assert1(msg.topic.split('/')[-1], msg.payload)
+        mqtt_client.assert2(msg.topic.split('/')[-1], msg.payload)
 
     def assert1(self, topic, msg):
         if topic == 'butler':
@@ -40,20 +41,24 @@ class Monitor(MQTTClient):
                     property1_dump.append(temp.get())
             elif action == 'arise' and phil_id == '0':
                 self.phil0_arise = True
-            elif action == 'sitdown' and phil_id == '0':
+
+    def assert2(self, topic, msg):
+        if topic == 'butler':
+            action, phil_id = msg.split(':')
+            if action == 'sit' and phil_id == '0' and not self.phil0_sitdown:
+                self.phil1_arise = False
                 self.phil0_sitdown = True
             elif action == 'arise' and phil_id == '1' and self.phil0_sitdown:
-                phil1_arise = True
+                self.phil1_arise = True
+                self.phil0_sitdown = False
             elif action == 'arise' and phil_id == '1' and not self.phil0_sitdown:
                 temp = copy.copy(last_ten_messages)
                 while not temp.empty():
                     property2_dump.append(temp.get())
-            elif action == 'sitdown' and phil_id == '0' and not self.phil1_arise:
+            elif action == 'sit' and phil_id == '0' and not self.phil1_arise:
                 temp = copy.copy(last_ten_messages)
                 while not temp.empty():
                     property2_dump.append(temp.get())
-            
-
 
 def monitor_cleanup(client):
     client.disconnect()
