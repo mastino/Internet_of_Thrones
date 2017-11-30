ff870e18646e4ae90eafe6cd474372802f8e476a
Brian Gravelle <gravelle@cs.uoregon.edu>
2017-11-29 22:43:39 -0800

    checkpoint 2 LTSA

--- final/ltsa/gen.py
+++ final/ltsa/gen.py
@@ -14,20 +14,27 @@ file.write("\n")
 file.write("\n")
 
 lower_priority = \
-"""{3}receive_request[{0}][ID][Lanes] -> send_permission[ID][{0}] -> WAIT_RESPONSE{1}[ll2][ll3][ll4][l] |
-{3}receive_permission[{0}][ID] -> WAIT_RESPONSE{2}[ll2][ll3][ll4][l] |
+"""{2}receive_request[{0}][ID][Lanes] -> send_permission[ID][{0}] -> WAIT_RESPONSE{1}[ll2][ll3][ll4][l] |
+"""
+
+lower_priority1 = \
+"""{2}receive_permission[{0}][ID] -> WAIT_RESPONSE{1}[ll2][ll3][ll4][l] |
 """
 
 low_list = ["", "[{}][hl3][hl4]","[hl2][{}][hl4]","[hl2][hl3][{}]"]
 
-for i in range(1,MAX_CARS):
+for i in range(1,MAX_CARS+1):
     file.write("\n")
     cnt = i
+    cnt1 = 1
     when_str = "when (ID == {}) ".format(i)
     for j in range(1,MAX_CARS+1):
         if i < j:
-            file.write(lower_priority.format(j, low_list[cnt].format("True"), low_list[cnt].format("False"), when_str))
+            file.write(lower_priority.format(j, low_list[cnt].format("True"), when_str))
             cnt+=1
+        if i != j:
+            file.write(lower_priority1.format(j, low_list[cnt1].format("False"), when_str))
+            cnt1+=1
 
 file.write("\ntime_out[ID] -> WAIT_PERMISSION[hl2][hl3][hl4][ll2][ll3][ll4][l]")
 
@@ -113,18 +120,44 @@ lower_priority = \
 """
 
 low_list2 = ["", "[{}][ll3][ll4]","[ll2][{}][ll4]","[ll2][ll3][{}]"]
-when_lst = ["", "ll2", "ll3", "ll4"]
+when_lst = ["", "ll2", "ll3", "ll4",""]
 
-for i in range(1,MAX_CARS):
+for i in range(1,MAX_CARS+1):
     file.write("\n")
     cnt = 1
     for j in range(1,MAX_CARS+1):
-        when_str = "when (ID != {} && {}) ".format(i, when_lst[cnt])
+        when_str = "when (ID == {} && {}) ".format(i, when_lst[cnt])
         if i != j:
             file.write(lower_priority.format(j, low_list2[cnt].format("False"),  when_str))
             cnt+=1
 
 
 file.write("""\nwhen(!ll2 && !ll3 && !ll4) restarting[ID] -> STARTUP |""")
+
+
+
+file.write("\n")
+file.write("\n")
+file.write("\n")
+file.write("sendall")
+file.write("\n")
+file.write("\n")
+file.write("\n")
+
+lower_priority = \
+"""{1} send_request[ID][{0}][l] -> SENDALL[a][b][c][cnt-1] |
+"""
+
+for i in range(1,MAX_CARS+1):
+    file.write("\n")
+    cnt = i
+    for j in range(1,MAX_CARS+1):
+        when_str = "when (ID == {} && cnt > 0) ".format(i)
+        if i != j:
+            file.write(lower_priority.format(j, when_str))
+            cnt+=1
+
+
+file.write("""\nwhen(cnt == 0) dummy[ID] -> BCAST""")
 
 file.close()
